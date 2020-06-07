678d3ef3f86da0a5622aaaa494beb308ae11e770
Vinícius Martins Pinto da Silva <vinicius.mp.silva@outlook.com>
2020-06-07 03:16:04 -0300

    Corrigindo Inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -11,28 +11,28 @@ const Home = () => {
   const navigation = useNavigation();
 
   function handleNavigateToPoints() {
-    navigation.navigate('Points', {uf, city});
+    navigation.navigate('Points', { uf, city });
   }
 
   return (
-    <KeyboardAvoidingView 
-      style={{ flex: 1 }} 
-      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-      keyboardVerticalOffset={-150}
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}
     >
+
       <ImageBackground
         source={require('../../assets/home-background.png')}
         style={styles.container}
         imageStyle={{ width: 274, height: 368 }}
       >
         <View style={styles.main}>
-            <Image source={require('../../assets/logo.png')} />
-            <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
-            <Text style={styles.description}>Ajudamos a encontrarem pontos de coleta de forma eficiente</Text>
+          <Image source={require('../../assets/logo.png')} />
+          <Text style={styles.title}>Seu marketplace de coleta de resíduos</Text>
+          <Text style={styles.description}>Ajudamos a encontrarem pontos de coleta de forma eficiente</Text>
         </View>
 
         <View style={styles.footer}>
-          <TextInput 
+          <TextInput
             style={styles.input}
             placeholder="Selecione a UF"
             value={uf}
